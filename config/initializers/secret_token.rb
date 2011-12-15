43a703f8fd1e989bfed2be1b01e694d3237a26c9
Kyle Shipley <shipstar@gmail.com>
2011-12-14 23:31:28 -0500

    Initial Rails commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shiplington::Application.config.secret_token = '5481895b544cd6e82d537e1809a24dd11da2084cb0fcce849d5902dcd223c63d6425bf6d326f5829b36d35e2462c34d6dd1d456e09c2b89952244af12dfc7fbf'
