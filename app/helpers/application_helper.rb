2b455dfdc374124b3565c58c71c5db3d7e2ea821
Kyle Shipley <shipstar@gmail.com>
2012-02-14 15:20:50 -0500

    Better page title. Add meta description tag.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def page_title
+    "Shiplington - An Indianapolis Web Development and Lean Startup Consultancy"
+  end
+
+  def meta_description
+    "Shiplington provides top-tier software development and startup advice " +
+    "based on extensive experience with early-stage startups and the web."
+  end
 end
